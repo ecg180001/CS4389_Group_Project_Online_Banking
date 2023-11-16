bf01ed287f9c5f8550d337ccdfe9cc35a3ee3fda
Emmanuel Asante <34814886+MannyE@users.noreply.github.com>
2023-11-15 18:44:09 -0600

    Small changes

--- bankify/src/index.js
+++ bankify/src/index.js
@@ -1,29 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div class="index-body">
-    <section class="main-section">
-      <header class="nav-bar" className="main-header">
-        <h3 class="logo">Bankify</h3>
-        <nav>
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <a href="#">Services</a>
-        </nav>
-      </header>
+      <section class="main-section">
+        <header class="nav-bar" className="main-header">
+          <h3 class="logo">Bankify</h3>
+          <nav>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Services</a>
+          </nav>
+        </header>
 
-      <div class="hero-section">
-        <h1>Your Trusted Banking Partner</h1>
-        <img src="/" alt="" srcset="" />
-      </div>
+        <div class="hero-section">
+          <h1>Your Trusted Banking Partner</h1>
+          <img src="/" alt="" srcset="" />
+        </div>
 
-      <div class="buttons">
-        <a href="Login.html"><button>Login / Sign Up</button></a>
-      </div>
-    </section>
-  </div>
+        <div class="buttons">
+          <a href="Login.html">
+            <button>Login / Sign Up</button>
+          </a>
+        </div>
+      </section>
+    </div>
   );
 }
 
