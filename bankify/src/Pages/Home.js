57e7c94cb2ab305370d092afbd2f08512cd14991
Emmanuel G <127249203+ecg180001@users.noreply.github.com>
2023-11-15 18:42:25 -0600

    Convert html to react components

---
+++ bankify/src/Pages/Home.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import '../Assets/style.css';
+
+function Home() {
+  return (
+    <div class="index-body">
+    <section class="main-section">
+      <header class="nav-bar" className="main-header">
+        <h3 class="logo">Bankify</h3>
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Services</a>
+        </nav>
+      </header>
+
+      <div class="hero-section">
+        <h1>Your Trusted Banking Partner</h1>
+        <img src="/" alt="" srcset="" />
+      </div>
+
+      <div class="buttons">
+        <a href="Login.html"><button>Login / Sign Up</button></a>
+      </div>
+    </section>
+  </div>
+  );
+}
+
+export default Home;
