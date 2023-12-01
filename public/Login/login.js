b996ee9b4d6590da028455df04416903d5b6325b
Emmanuel G <127249203+ecg180001@users.noreply.github.com>
2023-11-30 23:06:11 -0600

    Added sign in / sign up validation

--- public/Login/login.js
+++ public/Login/login.js
@@ -1,14 +1,97 @@
 // Retrieve User Information
-
 let createUserButton = document.getElementById("create_user");
-createButton.addEventListener("click", createUser);
-let createUser = (e) => {
-  e.preventDefault();
-  alert("Create User");
+let signInButton = document.getElementById("sign-in");
+
+// Rerieve Username, Password and email elements
+let emailElement = document.getElementById("email");
+let passwordElement = document.getElementById("password");
+let confirmElement = document.getElementById ("confirm_password");
+
+let emailValue = '';
+let passwordValue = '';
+let confirmValue = '';
+
+emailElement.addEventListener("keydown", (e) => {
+    emailValue = e.target.value
+})
+
+passwordElement.addEventListener("keyup", (e) => {
+    passwordValue = e.target.value
+})
+
+confirmElement.addEventListener("keyup", (e) => {
+    confirmValue = e.target.value
+})
+
+// Function to create new User
+let createNewUser = (e) => {
+    e.preventDefault();
+    
+    console.log(emailValue)
+    console.log(passwordValue)
+    console.log(confirmValue)
+    // 1: Validate Password
+    if (!validatePassword(passwordValue))
+        alert("Password not valid")
+
+    // 2: Ensure passwords match
+    else if (passwordValue!=confirmValue)
+        alert("Passwords don't match")  
+    
+
+    
+    // console.log(email.value);
+    // console.log(password.value);
+    // console.log(confirm.value); 
+    
+    else 
+        window.location.href = './Create';    
+    
+        // Password Validation code
+    function validatePassword(password) {
+      // Minimum length of 8 characters
+      if (password.length < 8) {
+          return false;
+      }
+
+      // At least one uppercase letter
+      if (!/[A-Z]/.test(password)) {
+          return false;
+      }
+
+      // At least one lowercase letter
+      if (!/[a-z]/.test(password)) {
+          return false;
+      }
+
+      // At least one digit
+      if (!/\d/.test(password)) {
+          return false;
+      }
+
+      // At least one special character (you can customize this list)
+      if (!/[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(password)) {
+          return false;
+      }
+
+      // If all conditions are met, the password is valid
+      return true;
+    }
 }
 
 
+// Function to Sign-In User
+let signInUser = (e) => {
+    e.preventDefault();
+    //retrive email and password
+    let email = document.getElementById("email-input");
+    let password = document.getElementById("password-input");
+    console.log(email.value);
+    console.log(password.value);
+}
 
+createUserButton.addEventListener("click", createNewUser);
+signInButton.addEventListener("click", signInUser); 
 
 // Initializing Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
@@ -34,8 +117,8 @@ initializeApp(firebaseConfig);
 
 // Function to log in the user
 function login() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+  const email = document.getElementById("email-input").value;
+  const password = document.getElementById("password-input").value;
 
   alert(email);
   alert(password);
@@ -61,33 +144,32 @@ function login() {
     });
 }
 
-// function createUser() {
-//   let user = {
-//     user_email: "test.user@example.com",
-//     user_password: "password",
-//     first_name: "John2",
-//     last_name: "Doe2",
-//     address: "123 Main St",
-//     initial_deposit: 1000
-//   };
+// // function createUser() {
+// //   let user = {
+// //     user_email: "test.user@example.com",
+// //     user_password: "password",
+// //     first_name: "John2",
+// //     last_name: "Doe2",
+// //     address: "123 Main St",
+// //     initial_deposit: 1000
+// //   };
 
-  // Submit user to back-end
-  axios
-    .post("http://localhost:8080/signup", user)
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
+// //   //Submit user to back-end
+// //   axios
+// //     .post("http://localhost:8080/signup", user)
+// //     .then(function (response) {
+// //       console.log(response);
+// //     })
+// //     .catch(function (error) {
+// //       console.log(error);
+// //     });
+// // }
 
-let createButton = document.getElementById("create_user");
-// function createUser() {
-//   alert("Create User");
-//   window.location.href = "/Create";
-// }
+// let createButton = document.getElementById("create_user");
+// // function createUser() {
+// //   alert("Create User");
+// //   window.location.href = "/Create";
+// // }
 
-createButton.addEventListener("click", createUser);
-
-alert("Hello World");
+// createButton.addEventListener("click", createUser);
+console.log("Hello World");
