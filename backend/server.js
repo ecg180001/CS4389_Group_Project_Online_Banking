3eb90699601aea4e1c05b83c5324e8623d132cf5
Emmanuel Asante <34814886+MannyE@users.noreply.github.com>
2023-12-04 22:18:07 -0600

    Revert "Merge branch 'frontend-merged' of https://github.com/ecg180001/CS4389_Group_Project_Online_Banking into frontend-merged"
    This reverts commit 796f49a20ba23b0bd30f363424a892129bdadb41, reversing
    changes made to 63e928475d2fb1bdc23ad06d218c6f1b753a21b2.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const admin = require('firebase-admin');
 const jwtUtil = require('./utils/jwtUtil')
-const cors = require('cors');
-
 const serviceAccount = require('./serviceAccount.json');
 const keygen = require('keygen');
 
@@ -12,7 +10,6 @@ admin.initializeApp({credential: admin.credential.cert(serviceAccount)});
 
 const app = express();
 const port = 8080;
-app.use(cors());
 
 app.use(express.json());
 
